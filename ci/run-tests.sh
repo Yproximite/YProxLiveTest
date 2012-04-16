2bec5e36d53b7a64039790e38b29eda03b4ea693
dantleech <dantleech>
2012-04-16 09:43:46 +0200

    Added response times test

--- ci/run-tests.sh
+++ ci/run-tests.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/sh
+rm -Rf output/*
+php bin/test.php test:response-time --write-errors=`pwd`/output/errors --write-response-times=`pwd`/output/response http://admin.plombierweb.fr/platformmap.xml
 php bin/test.php test:screenshots http://admin.plombierweb.fr/platformmap.xml output/
