4aebfa504daecfc93d4ea1a18f21ba93bd7ccfb5
dantleech <dantleech>
2012-04-17 09:54:01 +0200

    Fixes

--- ci/run-tests.sh
+++ ci/run-tests.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/sh
 OUTPUT_DIR="`pwd`/output"
 rm -Rf output/*
-php bin/test.php test:response-time --host-filter="&eden&" --write-errors=$OUTPUT_DIR"/errors.log" --write-response-times=$OUTPUT_DIR"/response.log" http://admin.plombierweb.fr/platformmap.xml
+php bin/test.php test:response-time --write-errors="$OUTPUT_DIR/errors.log" --write-response-times="$OUTPUT_DIR/response.log" http://admin.plombierweb.fr/platformmap.xml
 cat $OUTPUT_DIR"/response.log" | perl -pe "s/([0-9\.]+).*/\$1/g" | awk '{total = total + $1; lines++}END{ print total / lines}' > $OUTPUT_DIR"/average-response.txt"
 # php bin/test.php test:screenshots http://admin.plombierweb.fr/platformmap.xml output/
