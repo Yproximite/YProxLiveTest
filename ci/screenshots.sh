f69cd0c4f1bb5837164165364ac498147a50e035
dantleech <dantleech>
2012-04-30 12:48:21 +0200

    Fided

--- ci/screenshots.sh
+++ ci/screenshots.sh
@@ -14,7 +14,7 @@ fi
 
 rm $OUTPUT_DIR/original/*
 
-php bin/test.php test:screenshots --limit=100 http://admin.plombierweb.fr/platformmap.xml $OUTPUT_DIR/original
+php bin/test.php test:screenshots http://admin.plombierweb.fr/platformmap.xml "$OUTPUT_DIR/original"
 #php bin/test.php test:screenshots --use-base-url=http://yprox.localhost http://yprox.localhost/platformmap.xml $OUTPUT_DIR/original
-mogrify -auto-orient  -thumbnail 400x400 -unsharp 0x.5 -path $OUTPUT_DIR/$THUMB_SIZE $OUTPUT_DIR/original/*.png
+mogrify -auto-orient  -thumbnail 400x400 -unsharp 0x.5 -path "$OUTPUT_DIR/$THUMB_SIZE" "$OUTPUT_DIR/original/*.png"
 xsltproc ci/screenshot.xsl output/screenshots/original/screenshots.xml > output/screenshots/index.html
