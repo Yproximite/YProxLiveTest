b5f723a2db93323b825acd3342eb91e45c64d91b
dantleech <dantleech>
2012-05-30 08:46:15 +0200

    Support for base_url parameter

--- ci/run-tests.sh
+++ ci/run-tests.sh
@@ -7,5 +7,5 @@ then
 fi
 
 rm -Rf output/*
-php bin/test.php test:response-time --host-filter="$1" --write-errors="$OUTPUT_DIR/errors.log" --write-response-times="$OUTPUT_DIR/response.log" http://admin.plombierweb.fr/platformmap.xml
+php bin/test.php test:response-time --use-base-url=YPROX_BASE_URL --host-filter="$1" --write-errors="$OUTPUT_DIR/errors.log" --write-response-times="$OUTPUT_DIR/response.log" http://admin.plombierweb.fr/platformmap.xml
 cat "$OUTPUT_DIR/response.log" | perl -pe "s/([0-9\.]+).*/\$1/g" | awk '{total = total + $1; lines++}END{ print total / lines}' > "$OUTPUT_DIR/average-response.txt"
